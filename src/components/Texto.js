9bd896129462d279464ff0ff9d4815dc06f2a937
Amalyem <ameliacallizaya328@gmail.com>
2024-01-18 11:51:12 -0400

    Last commit

--- src/components/Texto.js
+++ src/components/Texto.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import '../styles/Texto.css';
-import {useState} from React ;
+ 
 function  Texto() {
-    const[añadir, setAñadiendo]=useState([]);
-    const [click,setClick]=useState('no se añadio nada')
+    
     
 
     return(
